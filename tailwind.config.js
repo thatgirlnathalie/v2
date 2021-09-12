0c49b4da8e9f6a68f3925dad5212283a27d7f016
nameisnathalie <nathaliemsgn@gmail.com>
2021-09-12 12:10:13 +0400

    Added live chat feature, customized cursor

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,19 +47,23 @@ module.exports = {
         },
       },
       screens: {
-        "3xl": {"min": "1920px", "max": "2049px"},
-        "4xl": {"min": "2050px"},
+        "3xl": { min: "1520px", max: "1920px" },
+        "4xl": { min: "1921px" },
       },
     },
     container: {
       center: true,
-      padding: "1rem",
+      padding: {
+        DEFAULT: "1rem",
+        "3xl": "6rem",
+        "4xl": "8rem",
+      },
       screens: {
         lg: "900px",
         xl: "1100px",
         "2xl": "1100px",
-        "3xl": "1220px",
-        "4xl": "1380px",
+        "3xl": "1520px",
+        "4xl": "1920px",
       },
     },
   },
